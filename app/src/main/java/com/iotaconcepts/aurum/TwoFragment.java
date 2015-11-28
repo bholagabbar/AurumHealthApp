be3e3db19faef1ed55df44a770d6a74137f128fd
bholagabbar <shreyans.sheth@gmail.com>
2015-11-28 20:10:19 +0530

    Added Location

--- app/src/main/java/com/iotaconcepts/aurum/TwoFragment.java
+++ app/src/main/java/com/iotaconcepts/aurum/TwoFragment.java
@@ -1,12 +1,12 @@
 package com.iotaconcepts.aurum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.iotaconcepts.aurum.R;
+import android.widget.Button;
 
 
 public class TwoFragment extends Fragment
@@ -24,8 +24,17 @@ public class TwoFragment extends Fragment
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-
-        return inflater.inflate(R.layout.fragment_three, container, false);
+        final View myInflatedView = inflater.inflate(R.layout.fragment_two, container, false);
+        Button b1=(Button)myInflatedView.findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                startActivity(new Intent(getActivity(),MapsActivity.class));
+            }
+        });
+        return myInflatedView;
     }
 
 
